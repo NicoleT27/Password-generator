2483f26a40c162e08532a7e1a5967d271e28f159
Nicole <nicolejoan9@yahoo.com>
2023-07-17 09:16:19 -0400

    added pseudo code for organization, as well as defined generatepassword function with 1st prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,27 @@
-// Assignment Code
+// prompt the user to ask for the length between 8-128
+// prompt which character types such as lowercase, uppercase, numeric, and spcial characters indivisually , seperate prompts
+// atleast one chracter type should be selected doesnt have to be all
+// then new password is generated
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// defines generate password function and provides message for no response
+function generatePassword() {
+  console.log("Button has been clicked");
+  // prompt the user for password criteria
+  var length = prompt(
+    "how many characters would you like your password to contain?"
+  );
+  if (length < 8 || length > 128) {
+    alert("Please pick a number between 8 and 128");
+    return;
+  }
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
