d08c20717adedd59e5730c7affa21b9f9b1229e7
Nicole <nicolejoan9@yahoo.com>
2023-07-17 16:18:35 -0400

    added strings of data for upper/lower case,numeric and special characters & their confirmation questions for each one

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // defines generate password function and provides message for no response
+
 function generatePassword() {
   console.log("Button has been clicked");
   // prompt the user for password criteria
@@ -16,6 +17,22 @@ function generatePassword() {
     alert("Please pick a number between 8 and 128");
     return;
   }
+  var uppercase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+  uppercase = confirm(
+    "Do you want uppercase characters? Click Ok for YES and Cancel for NO"
+  );
+  var lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+  lowercase = confirm(
+    "Do you want lowercase characters? Click Ok for YES and Cancel for NO"
+  );
+  var numbers = "0,1,2,3,4,5,6,7,8,9";
+  numbers = confirm(
+    "Do you want numeric characters? Click Ok for YES and Cancel for NO"
+  );
+  var special = " !,#,$,%,&,',(,),*,+,-,.,/,:,;,<,=,>,?,@,[,,],^,_,`,{,|,},~";
+  special = confirm(
+    "Do you want special characters? Click Ok for YES and Cancel for NO"
+  );
 }
 
 function writePassword() {
